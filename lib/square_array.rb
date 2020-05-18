c65157dafe1002a5893191197fe97a603572664c
Jodi Croft <jodipruett@gmail.com>
2020-05-18 18:59:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
 new_array = []
   array.length.times do |index|
-    new_array.push(array[index]**)
+    new_array.push(array[index] * array[index])
   end
   new_array
 end
